d44ef57a8d4b010312d92e14fccc5e1db8e306b7
neurot <casanova.raphael@gmail.com>
2022-11-22 13:58:11 +0100

    add higher order functions

--- chapter_02/lib/higher_order_functions.dart
+++ chapter_02/lib/higher_order_functions.dart
@@ -1,3 +1,12 @@
+// see also 101 linq samples in dart
+void higherOrderFunctionsPlayground() {
+  mapping();
+  sorting();
+  filtering();
+  reducing();
+  flattening();
+}
+
 List<Map> data = [
   {'first': 'Nada', 'last': 'Mueller', 'age': 10},
   {'first': 'Kurt', 'last': 'Gibbons', 'age': 9},
@@ -21,8 +30,50 @@ List<Map> data = [
   {'first': 'Taio', 'last': 'Mathews', 'age': 17},
 ];
 
-void higherOrderFunctionsPlayground() {
-  final names = mapping();
+void flattening() {
+  final matrix = [
+    [1, 0, 0],
+    [0, 0, -1],
+    [0, 1, 0],
+  ];
+
+  print('');
+  final linear = matrix.expand<int>((row) => row);
+  print(linear);
+}
+
+void reducing() {
+  final allAges = data.map<int>((person) => person['age']);
+  final total = allAges.reduce((total, age) => total + age);
+  final average = total / allAges.length;
+
+  print('');
+  print('The average age is $average');
+
+  // fold -> start total with -1000
+  // final oddTotal = allAges.fold<int>(-1000, (total, age) => total + age);
+}
+
+void filtering() {
+  final onlyMs = doMap().where((name) => name.last.startsWith('M'));
+
+  print('');
+  print('Filtered List by "starts with M');
+  onlyMs.forEach(print);
+}
+
+void sorting() {
+  final names = doMap();
+
+  names.sort((a, b) => a.last.compareTo(b.last));
+
+  print('');
+  print('Alphabetical List of Names:');
+  names.forEach(print);
+}
+
+void mapping() {
+  final names = doMap();
   names.forEach(print);
 }
 
@@ -38,12 +89,10 @@ class Name {
   }
 }
 
-List<Name> mapping() {
+List<Name> doMap() {
   // Transform the data from raw maps to a strongly typed model
   final names = data.map<Name>((Map rawName) {
-    final first = rawName['first'];
-    final last = rawName['last'];
-    return Name(first, last);
+    return Name(rawName['first'], rawName['last']);
   }).toList();
 
   return names;
