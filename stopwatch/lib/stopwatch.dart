e221252f1aaa210dd9c46d2c5d52c3e4042056cc
neurot <casanova.raphael@gmail.com>
2022-11-25 22:50:09 +0100

    add button logic

--- stopwatch/lib/stopwatch.dart
+++ stopwatch/lib/stopwatch.dart
@@ -11,19 +11,39 @@ class StopWatch extends StatefulWidget {
 
 class StopWatchState extends State<StopWatch> {
   int seconds = 0;
-  late Timer timer;
+  // Timer timer = Timer.periodic(const Duration(seconds: 1), (timer) {});
+  Timer? timer;
+  bool isTicking = true;
 
   @override
-  void initState() {
-    super.initState();
-
-    seconds = 0;
-    timer = Timer.periodic(const Duration(seconds: 1), _onTick);
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
   }
 
   void _onTick(Timer timer) {
     setState(() {
       ++seconds;
+    });
+  }
+
+  void _startTimer() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      _onTick,
+    );
+
+    setState(() {
+      seconds = 0;
+      isTicking = true;
+    });
+  }
+
+  void _stopTimer() {
+    timer?.cancel();
+
+    setState(() {
+      isTicking = false;
     });
   }
 
@@ -33,20 +53,47 @@ class StopWatchState extends State<StopWatch> {
       appBar: AppBar(
         title: const Text('Stopwatch'),
       ),
-      body: Center(
-        child: Text(
-          '$seconds ${_secondsText()}',
-          style: Theme.of(context).textTheme.headline5,
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            '$seconds ${_secondsText()}',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                ),
+                onPressed: isTicking ? null : _startTimer,
+                child: const Text('Start'),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                ),
+                onPressed: isTicking ? _stopTimer : null,
+                child: const Text('Stop'),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
 
   String _secondsText() => seconds == 1 ? 'second' : 'seconds';
-
-  @override
-  void dispose() {
-    timer.cancel();
-    super.dispose();
-  }
 }
